2f57febab8329d7b87c41b3793edef3f92bfa03e
Andre Souza <andre152803@outlook.com>
2023-06-25 17:44:28 -0300

    feat: create base home

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [all, setAll] = useState([
+    {
+      id: 1,
+      text: "Criar projeto em Angular com db.json",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Estudar js",
+      category: "Estudos",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Montar carro turbo",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1>To Do List </h1>
+      <div className="todo-list">
+        {all.map((item) => (
+          <div className="todo" key={item.id}>
+            <div className="content">
+              <p>{item.text}</p>
+              <div className="category"> ({item.category})</div>
+            </div>
+            <>
+              <button>Completar</button>
+              <button>Delete</button>
+            </>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
